bb3da4cfd662484063f31c4c9a1d418ccb59f661
SagarSingh1324 <sagarsinghprj@gmail.com>
2022-09-30 14:42:59 +0530

    adding README file

---
+++ Array/subarray_wmax_sum.cpp
@@ -0,0 +1,61 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  subarray_wmax_sum.cpp
+ *
+ *    Description:  find the subarray with maximum sum  
+ *
+ *        Version:  1.0
+ *        Created:  30/09/22 09:30:24 AM IST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Sagar Singh (SagarSingh1324), sagarsinghprj@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include<iostream>
+#include<vector>
+ 
+//_____secondary__functions______//
+
+int max_sub(std::vector<int> &arr)
+{
+    int max_so_far = 0, max_ending_here = 0;
+ 
+    for (int i = 0; i < arr.size(); i++) {
+        max_ending_here = max_ending_here + arr[i];
+        if (max_so_far < max_ending_here)
+            max_so_far = max_ending_here;
+ 
+        if (max_ending_here < 0)
+            max_ending_here = 0;
+    }
+    return max_so_far;
+} 
+ 
+//______main__function_____//
+ 
+int main()
+{
+
+    std::vector<int>  arr;
+
+    arr.push_back(-2);
+    arr.push_back(1);
+    arr.push_back(-3);
+    arr.push_back(4);
+    arr.push_back(-1);
+    arr.push_back(2);
+    arr.push_back(1);
+    arr.push_back(-5);
+    arr.push_back(4);
+
+    int res =max_sub(arr);
+
+    std::cout<<res<<std::endl;
+
+    return 0;
+}
---
+++ next_array_permutation.cpp
@@ -0,0 +1,47 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  next_array_permutation.cpp
+ *
+ *    Description: find the 'next' permutation of an array 
+ *
+ *        Version:  1.0
+ *        Created:  29/09/22 11:51:58 AM IST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Sagar Singh (SagarSingh1324), sagarsinghprj@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+// A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+
+// For example, for arr = [1,2,3], the following are all the permutations of arr: [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
+
+
+#include<iostream>
+#include<bits/stdc++.h>
+ 
+//_____secondary__functions______//
+ 
+
+ 
+ 
+//______main__function_____//
+ 
+int main()
+{
+ 
+return 0; 
+}
+
+
+// The next permutation of an array of integers is the next lexicographically greater permutation of its integer. 
+// More formally, if all the permutations of the array are sorted in one container according to their lexicographical order, 
+// then the next permutation of that array is the permutation that follows it in the sorted container. 
+// If such arrangement is not possible, the array must be rearranged as the lowest possible order (i.e., sorted in ascending order).
+
+// For example, the next permutation of arr = [1,2,3] is [1,3,2].
